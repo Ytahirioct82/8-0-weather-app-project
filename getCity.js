af16e396733e0cb1d555a31e3715c0d95dcdcc8b
Younes Tahiri <you@example.com>
2021-10-10 17:47:36 -0400

    added average, min, and max temp for the three days

--- getCity.js
+++ getCity.js
@@ -1,19 +1,77 @@
+// ........extracts the name of the area....
 const getCity = (weather) => {
   const city = weather.nearest_area[0].areaName[0].value;
   return city;
 };
 
+// ........extracts the feels like temperature....
 const getCityTemp = (weather) => {
   const temp = weather.current_condition[0].FeelsLikeF;
   return temp;
 };
 
+// ........extracts the name of the region....
 const getRegion = (weather) => {
   const region = weather.nearest_area[0].region[0].value;
   return region;
 };
 
+// ........extracts the name of the country....
 const getCountry = (weather) => {
   const country = weather.nearest_area[0].country[0].value;
   return country;
+};
+
+// ........extracts today average temperature....
+const getTodayAverage = (weather) => {
+  const today = weather.weather[0].avgtempF;
+  return today;
+};
+
+// ........extracts today maximum temperature....
+const getTodayMax = (weather) => {
+  const today = weather.weather[0].maxtempF;
+  return today;
+};
+
+// ........extracts today minimum temperature....
+const getTodayMin = (weather) => {
+  const today = weather.weather[0].mintempF;
+  return today;
+};
+
+// ........extracts tomorrow average temperature....
+const getTomorrowAverage = (weather) => {
+  const tomorrow = weather.weather[1].avgtempF;
+  return tomorrow;
+};
+
+// ........extracts tomorrow maximum temperature....
+const getTomorrowMax = (weather) => {
+  const tomorrow = weather.weather[1].maxtempF;
+  return tomorrow;
+};
+
+// ........extracts tomorrow minimum temperature....
+const getTomorrowMin = (weather) => {
+  const tomorrow = weather.weather[1].mintempF;
+  return tomorrow;
+};
+
+// ........extracts day after tomorrow average temperature....
+const getDayAfterAverage = (weather) => {
+  const dayAfter = weather.weather[2].avgtempF;
+  return dayAfter;
+};
+
+// ........extracts day after tomorrow maximum temperature....
+const getDayAfterMax = (weather) => {
+  const dayAfter = weather.weather[2].maxtempF;
+  return dayAfter;
+};
+
+// ........extracts day after tomorrow minimum temperature....
+const getDayAfterMin = (weather) => {
+  const dayAfter = weather.weather[2].mintempF;
+  return dayAfter;
 };
--- main.js
+++ main.js
@@ -66,6 +66,120 @@ document.querySelector("#search-form").addEventListener("submit", (event) => {
         location.append(currentParagraph);
 
         resultArea.append(location);
+
+        const forecastResult = document.querySelector(".forecast");
+
+        while (forecastResult.firstChild) {
+          forecastResult.removeChild(forecastResult.firstChild);
+        }
+        // ...........TODAY FORECAST..........
+        const today = document.createElement("h3");
+        today.textContent = "Today";
+
+        // ...........TOMORROW FORECAST..........
+        const tomorrow = document.createElement("h3");
+        tomorrow.textContent = "Tomorrow";
+
+        // ...........DAY AFTER TOMORROW FORECAST..........
+        const afterTomorrow = document.createElement("h3");
+        afterTomorrow.textContent = "Day After Tomorrow";
+
+        forecastResult.append(today);
+        forecastResult.append(tomorrow);
+        forecastResult.append(afterTomorrow);
+
+        // ...........TODAY TEMPERATURE RESULT..........
+        const todayResult = document.createElement("div");
+        todayResult.classList.add("today");
+
+        // today's average temperature
+        const todayAverageH = document.createElement("h4");
+        todayAverageH.textContent = "Average Temperature:";
+        todayResult.append(todayAverageH);
+        const todayAverageP = document.createElement("p");
+        todayAverageP.textContent = `${getTodayAverage(weather)}°F`;
+        todayResult.append(todayAverageP);
+
+        // today's max temperature
+        const todayMaxH = document.createElement("h4");
+        todayMaxH.textContent = "Max Temperature:";
+        todayResult.append(todayMaxH);
+        const todayMaxP = document.createElement("p");
+        todayMaxP.textContent = `${getTodayMax(weather)}°F`;
+        todayResult.append(todayMaxP);
+
+        // today's minimum temperature
+        const todayMinH = document.createElement("h4");
+        todayMinH.textContent = "Min Temperature:";
+        todayResult.append(todayMinH);
+        const todayMinP = document.createElement("p");
+        todayMinP.textContent = `${getTodayMin(weather)}°F`;
+        todayResult.append(todayMinP);
+        //
+
+        //
+
+        // ...........TOMORROW TEMPERATURE RESULT..........
+        const tomorrowResult = document.createElement("div");
+        tomorrowResult.classList.add("tomorrow");
+
+        // tomorrow,s average temperature
+        const tomorrowAverageH = document.createElement("h4");
+        tomorrowAverageH.textContent = "Average Temperature:";
+        tomorrowResult.append(tomorrowAverageH);
+        const tomorrowAverageP = document.createElement("p");
+        tomorrowAverageP.textContent = `${getTomorrowAverage(weather)}°F`;
+        tomorrowResult.append(tomorrowAverageP);
+
+        // tomorrow's max temperature
+        const tomorrowMaxH = document.createElement("h4");
+        tomorrowMaxH.textContent = "Max Temperature:";
+        tomorrowResult.append(tomorrowMaxH);
+        const tomorrowMaxP = document.createElement("p");
+        tomorrowMaxP.textContent = `${getTomorrowMax(weather)}°F`;
+        tomorrowResult.append(tomorrowMaxP);
+
+        // tomorrow's minimum temperature
+        const tomorrowMinH = document.createElement("h4");
+        tomorrowMinH.textContent = "Min Temperature:";
+        tomorrowResult.append(tomorrowMinH);
+        const tomorrowMinP = document.createElement("p");
+        tomorrowMinP.textContent = `${getTomorrowMin(weather)}°F`;
+        tomorrowResult.append(tomorrowMinP);
+
+        //
+        //
+        // ...........DAY AFTER TOMORROW TEMPERATURE RESULT..........
+        const dayAfterResult = document.createElement("div");
+        dayAfterResult.classList.add("dayAfter");
+
+        // day after tomorrow average temperature
+        const dayAfterAverageH = document.createElement("h4");
+        dayAfterAverageH.textContent = "Average Temperature:";
+        dayAfterResult.append(dayAfterAverageH);
+        const dayAfterAverageP = document.createElement("p");
+        dayAfterAverageP.textContent = `${getDayAfterAverage(weather)}°F`;
+        dayAfterResult.append(dayAfterAverageP);
+
+        // day after tomorrow max temperature
+        const dayAfterMaxH = document.createElement("h4");
+        dayAfterMaxH.textContent = "Max Temperature:";
+        dayAfterResult.append(dayAfterMaxH);
+        const dayAfterMaxP = document.createElement("p");
+        dayAfterMaxP.textContent = `${getDayAfterMax(weather)}°F`;
+        dayAfterResult.append(dayAfterMaxP);
+
+        // day after tomorrow minimum temperature
+        const dayAfterMinH = document.createElement("h4");
+        dayAfterMinH.textContent = "Min Temperature:";
+        dayAfterResult.append(dayAfterMinH);
+        const dayAfterMinP = document.createElement("p");
+        dayAfterMinP.textContent = `${getDayAfterMin(weather)}°F`;
+        dayAfterResult.append(dayAfterMinP);
+
+        forecastResult.append(todayResult);
+        forecastResult.append(tomorrowResult);
+        forecastResult.append(dayAfterResult);
       }
     })
     .catch();
